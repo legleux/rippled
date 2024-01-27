f8448724cb4150978593decf7870c5a64d6a3e5e
Michael Legleux <mlegleux@ripple.com>
2024-01-26 18:07:24 -0800

    local package build

--- Builds/containers/shared/update_sources.sh
+++ Builds/containers/shared/update_sources.sh
@@ -5,16 +5,8 @@ function error {
     exit 1
 }
 
-cd /opt/rippled_bld/pkg/rippled
+cd ..
 export RIPPLED_VERSION=$(egrep -i -o "\b(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-[0-9a-z\-]+(\.[0-9a-z\-]+)*)?(\+[0-9a-z\-]+(\.[0-9a-z\-]+)*)?\b" src/ripple/protocol/impl/BuildInfo.cpp)
 
-: ${PKG_OUTDIR:=/opt/rippled_bld/pkg/out}
+: ${PKG_OUTDIR:=pkgs}
 export PKG_OUTDIR
-if [ ! -d ${PKG_OUTDIR} ]; then
-    error "${PKG_OUTDIR} is not mounted"
-fi
-
-if [ -x ${OPENSSL_ROOT}/bin/openssl ]; then
-    LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OPENSSL_ROOT}/lib ${OPENSSL_ROOT}/bin/openssl version -a
-fi
-
