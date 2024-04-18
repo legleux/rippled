63080b43ebac2529dd4703dfe4a08af9925a7c52
Michael Legleux <mlegleux@ripple.com>
2024-04-17 22:30:20 -0700

    bump unsigned

--- src/ripple/protocol/impl/BuildInfo.cpp
+++ src/ripple/protocol/impl/BuildInfo.cpp
@@ -33,7 +33,7 @@ namespace BuildInfo {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "3.3.4-b7"
+char const* const versionString = "3.3.4-b8"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
