59c46c54c2ddb31fda3d1f5fa74844e258bcf5b1
Michael Legleux <mlegleux@ripple.com>
2025-10-02 22:01:09 -0700

    moved that file

--- pkgs/build.sh
+++ pkgs/build.sh
@@ -6,7 +6,7 @@ set -o xtrace
 repo_dir=$PWD
 set -a
 repo_name="rippled"
-pkgs_dir="${repo_dir}/ci/pkgs"
+pkgs_dir="${repo_dir}/pkgs"
 build_dir="${repo_dir}/build/${PKG}/packages"
 shared_files="${pkgs_dir}/shared"
 pkg_files="${pkgs_dir}/packaging/${PKG}"
